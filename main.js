e8d29348769bca5ee5db37ff163b43f40e5b63d2
chil0 <34295748+chil0@users.noreply.github.com>
2019-11-29 20:56:23 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
-
-const fibonacci = require('./lib/fibonacci');
-var len =20;
-for(; len--; )
-{
-    console.log(fibonacci(len))
-}
-console.log(fibonacci(1));
-
+// const fibonacci = require('./lib/fibonacci')
+// console.log(fibonacci(1));
